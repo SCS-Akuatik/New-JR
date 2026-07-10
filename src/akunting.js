442a0f23fb67a08bcb5d778c4c5e3e9fbff2c8e4
SCS-Akuatik <academyjagorenang@gmail.com>
2026-07-10 11:17:17 +0700

    Add files via upload

---
+++ src/akunting.js
@@ -0,0 +1,12 @@
+import { sb } from './config.js';
+
+// ... [PASTE SEMUA KODE DARI SOURCE 7 (SCRIPT AKUNTING) DI SINI] ...
+
+// Daftarkan ke Global Scope
+window.tambahAkunting = tambahAkunting;
+window.loadAkuntingAdmin = loadAkuntingAdmin;
+window.loadRekapAkunting = loadRekapAkunting;
+window.loadInvoiceTercetak = loadInvoiceTercetak;
+window.batalkanInvoice = batalkanInvoice;
+window.lunasiInvoice = lunasiInvoice;
+window.kirimReminder = kirimReminder;
