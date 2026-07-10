951d637b65f7873f5503a9dde55a8bdda1305fe8
SCS-Akuatik <academyjagorenang@gmail.com>
2026-07-10 11:16:21 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import CSS Tailwind
+import './style.css'; 
+
+// Import Ekosistem JS Jago Renang Academy
+import './js/config.js';
+import './js/app.js';
+import './js/login.js';
+import './js/owner.js';
+
+// --- IMPORT FILE YANG BARU SAJA KAMU BUAT ---
+import './js/akunting.js';
+import './js/beginner.js';
+import './js/coach.js';
+import './js/fee.js';
+import './js/katalog.js';
+import './js/parents.js';
+import './js/siswa.js';
