4ddb0690a5aa993b9f9a7ee3665719b007807c4f
SCS-Akuatik <academyjagorenang@gmail.com>
2026-07-11 11:07:42 +0700

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 // Import CSS Tailwind
 import './style.css'; 
 
-// FASE 1, 2, 3: DASAR, ETALASE, ADMIN BEGINNER, LOGIN
+// FASE 1-3: DASAR, LOGIN, BEGINNER, COACH
 import './js/config.js';
 import './js/helper.js'; 
 import './js/app.js';
 import './js/beginner.js'; 
 import './js/login.js';
-
-// KITA BANGUNIN KULI COACH SEKARANG 👇
 import './js/coach.js'; 
 
-// Sisa kuli masih tidur...
+// KITA BANGUNIN 4 KULI ADMIN SISA 👇
+import './js/katalog.js';
+import './js/siswa.js';
+import './js/fee.js';
+import './js/akunting.js';
+
+// Kuli Owner & Parent nyusul besok...
 // import './js/owner.js';
-// import './js/akunting.js';
-// import './js/fee.js';
-// import './js/katalog.js';
 // import './js/parents.js';
-// import './js/siswa.js';
