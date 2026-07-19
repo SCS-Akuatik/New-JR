16fc467871574ee66e1b921e59de9dbe15bee87b
SCS-Akuatik <Academyjagorenang@gmail.com>
2026-07-19 19:10:08 +0700

    Update login page and logo

--- src/main.js
+++ src/main.js
@@ -12,3 +12,33 @@ import './js/fee.js';
 import './js/akunting.js';
 // KULI KASTA TERTINGGI (BOS BESAR) MASUK SINI 👇
 import './js/owner.js';
+// Import kode HTML dari file admin
+import adminHtml from './components/admin.html?raw';
+
+// Suntikkan kode tersebut ke dalam wadah di index.html
+document.getElementById('admin-container').innerHTML = adminHtml;
+// Import kode HTML Coach
+import coachHtml from './components/coach.html?raw';
+
+// Suntikkan ke container di index.html
+document.getElementById('coach-container').innerHTML = coachHtml;
+// Import kode HTML Parent
+import parentHtml from './components/parent.html?raw';
+
+// Suntikkan ke container di index.html
+document.getElementById('parent-container').innerHTML = parentHtml;
+// Import kode HTML Owner
+import ownerHtml from './components/owner.html?raw';
+
+// Suntikkan ke container di index.html
+document.getElementById('owner-container').innerHTML = ownerHtml;
+// Import kode HTML Non-Member
+import nonmemberHtml from './components/nonmember.html?raw';
+
+// Suntikkan ke container di index.html
+document.getElementById('nonmember-container').innerHTML = nonmemberHtml;
+// Import kode HTML Modal
+import modalHtml from './components/modal.html?raw';
+
+// Suntikkan ke container di index.html
+document.getElementById('modal-container').innerHTML = modalHtml;
