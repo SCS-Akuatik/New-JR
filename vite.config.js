d12825819b631e8045c7f50a816ccc8c57bc0480
JagoAcademy <academyjagorenang@gmail.com>
2026-07-25 14:14:16 +0700

    dr land

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path' // Wajib dipanggil untuk membaca letak file
 
 export default defineConfig({
   build: {
     outDir: 'dist', // Nanti pas di-build, semua file matang akan masuk ke folder ini
+    rollupOptions: {
+      input: {
+        // Jahitan 1: Halaman Utama
+        main: resolve(__dirname, 'index.html'),
+        
+        // Jahitan 2: Halaman Dryland (Pastikan dryland.html sejajar dengan index.html)
+        dryland: resolve(__dirname, 'dryland.html') 
+      }
+    }
   }
 })
